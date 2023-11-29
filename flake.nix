3774b5bc80398f95441e5d711d99b6fe1a4a2f71
jecaro <jeancharles.quillet@gmail.com>
2023-11-29 16:29:48 +0100

    cargo init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    let
+      derivation = pkgs:
+        let naersk' = pkgs.callPackage naersk { };
+        in
+        naersk'.buildPackage {
+          src = ./.;
+        };
+
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
+
+        in
+        {
+          # For `nix build` & `nix run`:
+          defaultPackage = derivation pkgs;
+
+          # For `nix develop`:
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs;
+              [
+                cargo
+                rust-analyzer
+                rustc
+                rustfmt
+              ];
+          };
+        }
+      ) // {
+      overlay = final: prev:
+        {
+          advent-of-code-2023 = derivation final;
+        };
+    };
+}
